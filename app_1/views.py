8a746a05b0a8dfbaee01b955aa28650e66e133e6
newyorkdeep <mpp14021972@gmail.com>
2026-06-04 11:34:59 +0300

    major update

--- app_1/views.py
+++ app_1/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .models import UserInput
 
-def form_page(request):
+def form_view(request):
+    if request.method == "POST":
+        form_dict = dict(request.POST)
+        form_dict.pop('csrfmiddlewaretoken', None)
+        polished_data={k: v[0] for k, v in form_dict.items()}
+        UserInput.objects.create(inputedData=polished_data)
+        return redirect('display_view')
     return render(request, 'form.html')
+
+def display_view(request):
+    allData=UserInput.objects.all().order_by('-creationDate')
+    return render(request, 'display.html', {'allData': allData})
+
+@require_POST
+def wipe_view(request):
+    UserInput.objects.all().delete()
+    return redirect('display_view')
--- hjoi32/urls.py
+++ hjoi32/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('form/', views.form_view, name='form_view'),     
+    path('display/', views.display_view, name='display_view'), 
+    path('wipe/', views.wipe_view, name='wipe_view'),
 ]
